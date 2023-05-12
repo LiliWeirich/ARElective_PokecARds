1f1de81c2dece6dc24ce3db9b8096dc6a4f22b95
Lili <lili.weirich@stud.h-da.de>
2023-05-12 12:54:00 +0200

    VuforaiLicense
    still trying to make everything work

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QWI0NXRtMy8vLy8vQUFBQm1kbml0cXNPZ0U0Y3M3eFdPanZ3RnZaRDRUdUl0STFPM1liOFg3MHd0QnliQWozVmRUQVRSOFM4Vmt3R0FndzI3OE9zQ2FFOUdkVGMxekhwQ0dGeUhnS0lHMGhtc0VtU2lpSnIvdFlXcFBOT29FZ3luTmwxczVmVUppOGNJcnhMZ21HNnpIejBHYXBEQzhOL3pWRVhOUCtrOWpMdFhTYUZNRDE2cUUvSlhHZTNQbnNPV2tCRWJvaEJoRmZraGcwb0hwMGNxRjI3NFZHYjFUcmpZOFU1RUhOZWV5cDVFRE1RK1Q0UTNoNGYxT2crM3RCTDdVTytEcEpKT1A3YUZLRkFROGgvMDgxRHc4RzBoSE1mWVhkaHBlVXp2bUZuaysxa3ZzOWdYNWtiQ3lFYjU3azc2ZzFYdXJoVGVCeDlRdWxKelM0eU5KSDNtNC9FUGZpU2k0Z1FuaGp5cURKN1ZzMEw3aS92QWlRajY4cHE=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
