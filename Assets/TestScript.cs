5b7f2492b9ab4fb0f777e95aa913e1b580de083d
LiliWeirich <lili.weirich@stud.h-da.de>
2023-06-11 11:32:28 +0200

    popup screen with clickable button
    need to rename stuff and change text

---
+++ Assets/TestScript.cs
@@ -0,0 +1,33 @@
+//================================================================
+// Darmstadt University of Applied Sciences, Expanded Realities
+// Course: Augmented Reality - Getting started in AR Foundation (Robin Bittlinger, Sebastian Schuchmann)
+// Script by:    Lili Weirich (769701)
+// Last changed: 11-06-23
+//================================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField] private ConfirmationWindow myConfirmationWindow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OpenComfirmationWindow("Welcome bla bla bla");
+    }
+
+    private void OpenComfirmationWindow(string message)
+    {
+        myConfirmationWindow.gameObject.SetActive(true);
+        myConfirmationWindow.yesButton.onClick.AddListener(YesClicked);
+        myConfirmationWindow.messageText.text = message;
+    }
+
+    private void YesClicked()
+    {
+        myConfirmationWindow.gameObject.SetActive(false);
+        Debug.Log("Yes clicked");
+    }
+  
+}
