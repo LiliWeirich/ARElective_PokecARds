1eab0c40ffe28b28414223b3a33e119283dbe9ac
Lili Weirich <lili.weirich@stud.h-da.de>
2023-06-24 13:38:06 +0200

    added berry button

---
+++ Assets/Scripts/feedingBerry.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class feedingBerry : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        //if button is pressed play animation (feeding kwaks)
+    }
+}
