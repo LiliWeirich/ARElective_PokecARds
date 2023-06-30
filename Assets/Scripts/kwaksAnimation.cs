36ebe65db7044d03a4754076adf5e38aafccb7d2
LiliWeirich <lili.weirich@stud.h-da.de>
2023-06-30 14:52:53 +0200

    kwaks animation plays as soon as click on berry icon

---
+++ Assets/Scripts/kwaksAnimation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kwaksAnimation : MonoBehaviour
+{
+    /*private Animator kwaksAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        kwaksAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (kwaksAnimator != null)
+        {
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                kwaksAnimation.SetTrigger("trHappy");
+            }
+        }
+    }
+    */
+
+    public GameObject kwaksModel; 
+
+    public void OnButtonClick()
+    {
+       kwaksModel.GetComponent<Animator>().Play("Armature|Happy");
+    }
+}
